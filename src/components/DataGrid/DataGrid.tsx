175c11d29fd6d94fd55c9afe9936640f618d13a2
huuquangnhatnguyen <nguye2hq@mail.uc.edu>
2025-02-12 01:58:12 -0500

    change proxy

--- src/components/DataGrid/DataGrid.tsx
+++ src/components/DataGrid/DataGrid.tsx
@@ -1,15 +1,13 @@
-// import * as React from "react";
+import { Typography } from "@mui/material";
+import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
-// import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import { Typography } from "@mui/material";
 import { GridDataType } from "../../@types/SynopsisData/grid.type";
-import "./styles.scss";
 import NameTag from "../NameTag/NameTag";
+import "./styles.scss";
 
 export default function DataGrid({ row }: { row: GridDataType }) {
   return (
